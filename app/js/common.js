932bed99e6c6c667f39d468fe405daad85ccf225
SvyatoslavMedov <SvyatoslavMedov1204@gmail.com>
2024-04-09 19:54:29 +0600

    Создание и модификация второго блока

--- app/js/common.js
+++ app/js/common.js
@@ -17,7 +17,32 @@ $(function() {
 		$('.hamburger').removeClass('is-active');
 	});
 
-	$('.carousel-services').owlCarousel();
+	$('.carousel-services').owlCarousel({
+		loop: true,
+		nav: true,
+		smartSpeed: 700,
+		navText: ['<i class="fa fa-angle-double-left"></i>','<i class="fa fa-angle-double-right"></i>'],
+		responsiveClass:true,
+		responsive:{
+			0:{
+                items:1
+            },
+            800:{
+                items:2
+            },
+            1100:{
+                items:3
+            }
+        }
+	});
+
+	function carouselService() {
+		$('.carousel-services-item').each(function() {
+			var ths = $(this),
+				thsh = ths.find('.carousel-services-content').outerHeight();
+				ths.find('.carousel-services-image').css('min-height', thsh);
+		});	
+	}carouselService();
 	
 });
 
